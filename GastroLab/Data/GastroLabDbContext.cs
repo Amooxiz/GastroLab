9eb13f53898be139ad826ba2985e9db61d648912
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2023-04-25 17:26:53 +0200

    Add project files.

---
+++ GastroLab/Data/GastroLabDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GastroLab.Data
+{
+    public class GastroLabDbContext : IdentityDbContext
+    {
+        public GastroLabDbContext(DbContextOptions<GastroLabDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ GastroLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GastroLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
